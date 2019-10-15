52bad9d8a0cabf5b7921ea23698fefd5272236ff
Geovani Pereira <geovanimac@hotmail.com>
2019-10-15 08:13:27 -0300

    Finalizando aplicação

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+import "./styles.css"; //importando css dentro do component, cada componente tem seu CSS
+
+const Header = () => <header id="main-header">JSHunt</header>
+
+
+
+export default Header;
